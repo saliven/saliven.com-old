0954f3465042b001807284f373183eb9eae0fe1d
saliven <s4liven@pm.me>
2021-12-26 20:47:38 +0100

    feat: add Repositories component

---
+++ src/components/sections/Repositories/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { PinnedRepo } from '../../../utils/types'
+import Repository from './Repository'
+
+export default function Repositories({ repos }: { repos: PinnedRepo[] }): JSX.Element {
+  return (
+    <section className="mt-16">
+      <h1 className="text-3xl font-semibold mb-4">
+        Repositories <span className="ml-3">📚</span>
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        {repos.map((repo, idx) => (
+          <Repository {...repo} key={idx} />
+        ))}
+      </div>
+    </section>
+  )
+}
