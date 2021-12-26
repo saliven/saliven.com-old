57aab163ea9786a3473662a5d9b516ec10e80478
saliven <s4liven@pm.me>
2021-12-26 20:47:05 +0100

    feat: add `getServerSideProps` function for github pinned repositories fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
+import { InferGetServerSidePropsType } from 'next'
+import React from 'react'
 import Introduction from '../components/sections/Introduction'
 import Technologies from '../components/sections/Technologies'
 import Contact from '../components/sections/Contact'
 import Footer from '../components/sections/Footer'
-import React from 'react'
 import Projects from '../components/sections/Projects'
+import { PinnedRepo } from '../utils/types'
 
-export default function Home(): JSX.Element {
+export default function Home({ pinnedRepos }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element {
   return (
     <div className="px-10 py-16 sm:px-20 md:px-40 xl:px-96 2xl:px-[32rem]">
       <Introduction />
@@ -15,4 +17,16 @@ export default function Home(): JSX.Element {
       <Footer />
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const data = await fetch('https://gh-pinned-repos.egoist.sh/?username=saliven').then(
+    async (response) => response.json() as Promise<PinnedRepo[]>
+  )
+
+  return {
+    props: {
+      pinnedRepos: data,
+    },
+  }
 }
