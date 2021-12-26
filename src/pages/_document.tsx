c02fe8a848814a31f1af2ee1382e410874767f01
saliven <s4liven@pm.me>
2021-12-26 20:44:46 +0100

    feat: add all Inter font weights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,10 @@ export default class AppDocument extends Document {
       <Html lang="en">
         <Head lang="en">
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
           <script
             async
             defer
