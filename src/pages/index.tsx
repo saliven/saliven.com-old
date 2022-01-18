9cc644065c0255721aa31e1e1e7710c5ad36fe0d
Saliven <s4liven@pm.me>
2022-01-18 22:30:26 +0100

    refactore: remove some components and `getStaticProps`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,12 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { PinnedRepo } from '../utils/types'
 import React from 'react'
-import Introduction from '../components/sections/Introduction'
-import Technologies from '../components/sections/Technologies'
-import Contact from '../components/sections/Contact'
-import Footer from '../components/sections/Footer'
-import Projects from '../components/sections/Projects'
-import Repositories from '../components/sections/Repositories'
+import Introduction from '../components/Introduction'
+import Contact from '../components/Contact'
 
-export default function Home({ pinnedRepos }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+export default function Home(): JSX.Element {
   return (
-    <div className="px-10 py-16 sm:px-20 md:px-40 xl:px-96 2xl:px-[32rem]">
+    <>
       <Introduction />
       <Contact />
-      <Projects />
-      <Repositories repos={pinnedRepos} />
-      <Technologies />
-      <Footer />
-    </div>
+    </>
   )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await fetch('https://gh-pinned-repos.egoist.sh/?username=saliven').then(
-    async (response) => response.json() as Promise<PinnedRepo[]>
-  )
-
-  return {
-    props: {
-      pinnedRepos: data,
-    },
-    revalidate: 1800,
-  }
 }
