d8d7314f787a4579f152a858bb7e2c740981034d
Saliven <s4liven@pm.me>
2022-01-21 19:14:52 +0100

    chore: add `preact` for production build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   reactStrictMode: true,
   poweredByHeader: false,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 }
